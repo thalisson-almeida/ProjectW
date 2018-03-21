930250c6d8aa6c3118af043644d4fd09b09b0901
Thalisson Christiano de Almeida <thalissonca@gmail.com>
2018-03-21 08:57:42 -0300

    Initial Commit

---
+++ ProjectW/app/src/main/java/com/thalisson/android/projectw/view_data.java
@@ -0,0 +1,13 @@
+package com.thalisson.android.projectw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class view_data extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_data);
+    }
+}
